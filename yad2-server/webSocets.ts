8909a82323d9cd299d06b8ffeaea111bac8d2b04
RomZil <zilber.rom@gmail.com>
2023-03-21 19:41:30 +0200

    add server side WS

---
+++ yad2-server/webSocets.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+let userCount = 0;
+
+io.on('connection', (socket) => {
+  userCount++;
+  io.emit('userCountUpdated', userCount);
+
+  socket.on('disconnect', () => {
+    userCount--;
+    io.emit('userCountUpdated', userCount);
+  });
+});
+
+httpServer.listen(4200, () => {
+  console.log('listening on *:3000');
+});
