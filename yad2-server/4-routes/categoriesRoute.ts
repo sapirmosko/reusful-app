c9f23d7631349f424023fdfba72a937b0533aa5f
Ron Soffer <ronso008@gmail.com>
2023-03-22 14:14:47 +0200

    removed unused

--- yad2-server/4-routes/categoriesRoute.ts
+++ yad2-server/4-routes/categoriesRoute.ts
@@ -1,24 +1,27 @@
-import express from 'express';
-import { addImageForCategorie,getAllCategories, getAmountOfProductsByCategories } from '../2-logic/categorieLogic';
+import express from "express";
+import {
+  addImageForCategorie,
+  getAllCategories,
+  getAmountOfProductsByCategories,
+} from "../2-logic/categorieLogic";
 
 export const CategoriesRoute = express.Router();
 
-CategoriesRoute.get('/categories', async (req, res) => {
-    const categories = await getAllCategories();
-    res.status(200).json(categories);
-})
+CategoriesRoute.get("/categories", async (req, res) => {
+  const categories = await getAllCategories();
+  res.status(200).json(categories);
+});
 
-CategoriesRoute.post('/categories/image', async (req:any, res:any) => {
-    const body = req.files;
-    await addImageForCategorie(body)
+CategoriesRoute.post("/categories/image", async (req: any, res: any) => {
+  const body = req.files;
+  await addImageForCategorie(body);
+});
 
-})
-
-CategoriesRoute.get('/categories/chart',async (req,res)=>{
-    try{
-        const results = await getAmountOfProductsByCategories();
-        res.status(200).json(results);
-    }catch(e){
-        res.status(400).json(e)
-    }
-})
+CategoriesRoute.get("/categories/chart", async (req, res) => {
+  try {
+    const results = await getAmountOfProductsByCategories();
+    res.status(200).json(results);
+  } catch (e) {
+    res.status(400).json(e);
+  }
+});
