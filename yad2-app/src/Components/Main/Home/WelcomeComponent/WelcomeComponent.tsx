14a5ac765a3d2e5ac4a342d222e75d90b4c9681e
sapirmosko <72347902+sapirmosko@users.noreply.github.com>
2023-03-23 16:43:14 +0200

    addeed title to bar chart

--- yad2-app/src/Components/Main/Home/WelcomeComponent/WelcomeComponent.tsx
+++ yad2-app/src/Components/Main/Home/WelcomeComponent/WelcomeComponent.tsx
@@ -1,21 +1,27 @@
 import "./WelcomeComponent.css";
-import vectorHome from '../../../../images/vectorHome.png'
+import vectorHome from "../../../../images/vectorHome.png";
 import BarChart from "../../../BarChart/BarChart";
 
 function WelcomeComponent(): JSX.Element {
-    return (
-        <div className="WelcomeComponent">
-            <div className="WelcomeComponentSen">
-                <h2>Reusful, Buy and Sell Reusful stuff.</h2>
-                <span>Score a deal and save a bundle with our website! Find gently used clothes, furniture, electronics, and more at a fraction of the cost. Shop unique collectibles and rare finds. </span>
-                <span id="WelcomeComponentSenCatchPhrase">Shop with confidence at our website.</span>
-            </div>
-            <div className="WelcomeComponentChart">
-                <BarChart />
-            </div>
-
-        </div>
-    );
+  return (
+    <div className="WelcomeComponent">
+      <div className="WelcomeComponentSen">
+        <h2>Reusful, Buy and Sell Reusful stuff.</h2>
+        <span>
+          Score a deal and save a bundle with our website! Find gently used
+          clothes, furniture, electronics, and more at a fraction of the cost.
+          Shop unique collectibles and rare finds.{" "}
+        </span>
+        <span id="WelcomeComponentSenCatchPhrase">
+          Shop with confidence at our website.
+        </span>
+      </div>
+      <div className="WelcomeComponentChart">
+        <h3>Today's Sales by Product Category</h3>
+        <BarChart />
+      </div>
+    </div>
+  );
 }
 
 export default WelcomeComponent;
